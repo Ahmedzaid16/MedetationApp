abf4d6f89c60c1ec8f5647a8044ec23cdf8ebd4a
Ahmed Zaid <84353686+Ahmedzaid16@users.noreply.github.com>
2025-05-31 18:36:38 +0300

    init

---
+++ app/src/main/java/com/example/medetationapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.medetationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.medetationapp.ui.HomeScreen
+import com.plcoding.medetationapp.ui.theme.MedetationAppTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+        MedetationAppTheme {
+            HomeScreen()
+        }
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/medetationapp/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.plcoding.medetationapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+
+@Composable
+fun MedetationAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
